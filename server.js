cdc887ec7efc3ab38d20ff479560e3c95a4cc69c
Arun Kumar <java.kumar.arun@gmail.com>
2026-02-13 13:39:50 +0530

    server is running

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ import express from "express";
 import multer from "multer";
 import cors from "cors";
 import dotenv from "dotenv";
-import pdfParse from "pdf-parse";
+// import pdfParse from "pdf-parse";
+import { PDFParse } from "pdf-parse";
 import Tesseract from "tesseract.js";
 import {GoogleGenerativeAI} from "@google/generative-ai";
 
+dotenv.config();
+const PORT = process.env.PORT;
+// console.log(PORT)
+// console.log("Type is: "+ typeof(express))
+
+const server = express()
+
+server.listen(PORT, function(){
+    console.log("App is running at: "+PORT)
+})
