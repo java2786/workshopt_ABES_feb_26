c160b70b6be9c242fd126ca60e4c971440c102a2
Arun Kumar <java.kumar.arun@gmail.com>
2026-02-13 13:19:52 +0530

    import is done

---
+++ server.js
@@ -0,0 +1,10 @@
+// import var from "package";
+
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+import dotenv from "dotenv";
+import pdfParse from "pdf-parse";
+import Tesseract from "tesseract.js";
+import {GoogleGenerativeAI} from "@google/generative-ai";
+
